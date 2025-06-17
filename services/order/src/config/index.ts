a4380afc54040769289cbe0f9eee6316d98ceaa6
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-06-18 05:32:55 +0900

    APIエンドポイントのポート番号を変更し、オーダーサービスとプロダクトサービスの設定を修正しました。また、注文データに新しいエントリを追加しました。

--- services/order/src/config/index.ts
+++ services/order/src/config/index.ts
@@ -10,9 +10,10 @@ if(process.env.NODE_ENV !== 'production') {
 }
 
 export default {
-    PORT: process.env.PORT || 8002,
+    PORT: process.env.PORT || 8003,
     DB: process.env.DB || '',
     MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL || 'amqp://localhost',
     USER_SERVICE_URL: process.env.USER_SERVICE_URL || 'http://localhost:8001',
-    PRODUCT_SERVICE_URL: process.env.PRODUCT_SERVICE_URL || 'http://localhost:8003'
+    PRODUCT_SERVICE_URL: process.env.PRODUCT_SERVICE_URL || 'http://localhost:8002',
+    ORDER_SERVICE_URL: process.env.ORDER_SERVICE_URL || 'http://localhost:8003'
 }
